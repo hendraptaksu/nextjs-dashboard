d82473b74f375d7f0e56246e005549a4825a38c0
Hendra Prasetya <hendra.prasetya@taksu.tech>
2024-10-08 11:54:36 +0800

    finish

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,27 @@ import { z } from "zod";
 import { sql } from "@vercel/postgres";
 import { redirect } from "next/navigation";
 import { revalidatePath } from "next/cache";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
+  }
+}
 
 export type State = {
   errors?: {
@@ -57,6 +78,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
   } catch (error: unknown) {
+    console.log(error);
     // If a database error occurs, return a more specific error.
     return {
       message: "Database Error: Failed to Create Invoice.",
@@ -99,6 +121,7 @@ export async function updateInvoice(
     WHERE id = ${id}
   `;
   } catch (error) {
+    console.log(error);
     return { message: "Database Error: Failed to Update Invoice." };
   }
 
@@ -111,6 +134,7 @@ export async function deleteInvoice(id: string) {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath("/dashboard/invoices");
   } catch (error) {
+    console.log(error);
     return { message: "Database Error: Failed to Delete Invoice." };
   }
 }
--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -1,6 +1,5 @@
 import { CheckIcon, ClockIcon } from "@heroicons/react/24/outline";
 import clsx from "clsx";
-import Font from "next/font";
 
 export default function InvoiceStatus({ status }: { status: string }) {
   return (
